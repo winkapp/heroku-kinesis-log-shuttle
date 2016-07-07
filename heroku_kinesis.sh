6c546152ed23585e0132d5b22c73accb973830ec
Gabe Ochoa <gabeochoa@gmail.com>
2016-07-07 18:18:51 -0400

    Adding the ability to send to a kinesis stream and second url

--- heroku_kinesis.sh
+++ heroku_kinesis.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-nc -lk -p 514 -e /bin/log-shuttle -verbose -logs-url $KINESIS_URL -max-line-length 32000 -batch-size 498 -input-format rfc5424 -kinesis-shards $KINESIS_SHARDS
+nc -lk -p 514 | tee >(/bin/log-shuttle -verbose -logs-url $KINESIS_URL -max-line-length 32000 -batch-size 498 -input-format rfc5424 -kinesis-shards $KINESIS_SHARDS) | \
+  /bin/log-shuttle -verbose -logs-url $OTHER_URL -max-line-length 32000 -batch-size 498 -input-format rfc5424
