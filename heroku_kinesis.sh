f188e7c61bda32e0a1d20b0dbebb530af838f68e
Gabe Ochoa <gabeochoa@gmail.com>
2016-07-11 17:57:50 -0500

    Moving batch to 150

--- heroku_kinesis.sh
+++ heroku_kinesis.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-socat -u udp-recv:514 - | /bin/log-shuttle -logs-url $KINESIS_URL -max-line-length 35000 -batch-size 498 -input-format rfc5424 -kinesis-shards $KINESIS_SHARDS
+socat -u udp-recv:514 - | /bin/log-shuttle -logs-url $KINESIS_URL -max-line-length 35000 -batch-size 150 -input-format rfc5424 -kinesis-shards $KINESIS_SHARDS
