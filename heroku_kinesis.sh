4eba3f2b63213d6fdc6424bd8267534870a46cb6
Gabe Ochoa <gabeochoa@gmail.com>
2016-07-11 14:36:00 -0500

    Moving batch size to 150

--- heroku_kinesis.sh
+++ heroku_kinesis.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nc -lk -p 514 -e /bin/log-shuttle -logs-url $KINESIS_URL -max-line-length 32000 -batch-size 1 -input-format rfc5424 -kinesis-shards $KINESIS_SHARDS
+nc -lk -p 514 -e /bin/log-shuttle -logs-url $KINESIS_URL -max-line-length 32000 -batch-size 150 -input-format rfc5424 -kinesis-shards $KINESIS_SHARDS
