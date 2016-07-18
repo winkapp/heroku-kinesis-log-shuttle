b416f292cd969eb6f8816d3d0f5c01c6706090c0
Gabe Ochoa <gabeochoa@gmail.com>
2016-07-18 18:03:54 -0400

    Trying a fifo pipe

--- heroku_kinesis.sh
+++ heroku_kinesis.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-nc -lk -p 514 -e tee >(/bin/log-shuttle -verbose -logs-url $KINESIS_URL -max-line-length 32000 -batch-size 150 -input-format rfc5424 -kinesis-shards $KINESIS_SHARDS) \
-  >(/bin/log-shuttle -verbose -logs-url $OTHER_URL -max-line-length 32000 -batch-size 498 -input-format rfc5424)
+# nc -lk -p 514 -e tee >(/bin/log-shuttle -verbose -logs-url $KINESIS_URL -max-line-length 32000 -batch-size 150 -input-format rfc5424 -kinesis-shards $KINESIS_SHARDS) \
+#   -e tee >(/bin/log-shuttle -verbose -logs-url $OTHER_URL -max-line-length 32000 -batch-size 498 -input-format rfc5424)
+
+mkfifo pipe
+tail -f pipe | /bin/log-shuttle -verbose -logs-url $KINESIS_URL -max-line-length 32000 -batch-size 150 -input-format rfc5424 -kinesis-shards $KINESIS_SHARDS &
+nc -lk -p 514 | tee pipe | /bin/log-shuttle -verbose -logs-url $OTHER_URL -max-line-length 32000 -batch-size 498 -input-format rfc5424
