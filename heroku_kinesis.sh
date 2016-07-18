10eacd611b7569cfbfde7f1e7f997f35bb48ef85
Gabe Ochoa <gabeochoa@gmail.com>
2016-07-18 18:16:31 -0400

    -e instead of a pipe

--- heroku_kinesis.sh
+++ heroku_kinesis.sh
@@ -5,4 +5,4 @@
 
 mkfifo pipe
 tail -f pipe | /bin/log-shuttle -verbose -logs-url $KINESIS_URL -max-line-length 32000 -batch-size 150 -input-format rfc5424 -kinesis-shards $KINESIS_SHARDS &
-nc -lk -p 514 | tee pipe | /bin/log-shuttle -verbose -logs-url $OTHER_URL -max-line-length 32000 -batch-size 498 -input-format rfc5424
+nc -lk -p 514 -e tee pipe | /bin/log-shuttle -verbose -logs-url $OTHER_URL -max-line-length 32000 -batch-size 498 -input-format rfc5424
