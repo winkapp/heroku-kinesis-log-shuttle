ba6add3e5caf61c2c305674dac0e0b591fb342eb
Jonathan Hosmer <jonathan@wink.com>
2016-09-06 17:11:11 -0400

    Making max line length and batch size variables instead of hard-coded values.

--- log_shuttle.sh
+++ log_shuttle.sh
@@ -4,7 +4,7 @@ python select_server.py \
     -v \
     -e /bin/log-shuttle \
         -logs-url $KINESIS_URL \
-        -max-line-length 32000 \
-        -batch-size 150 \
+        -max-line-length ${MAX_LINE_LENGTH:-32000} \
+        -batch-size ${BATCH_SIZE:-150} \
         -input-format rfc5424 \
         -kinesis-shards $KINESIS_SHARDS
